1cdfa46cd8424a456e12f39aefd190e40d7e4528
Rusbell Ruiz Portocarrero <65029620+rusbell-24@users.noreply.github.com>
2023-05-09 22:26:35 -0500

    Creación de clase Playlist
    Se creo la clase Playlist

---
+++ src/sofka/musicplayer/Playlist.java
@@ -0,0 +1,37 @@
+package sofka.musicplayer;
+
+
+import java.util.ArrayList;
+
+public class Playlist extends SortBy{
+
+    private ArrayList customList = new ArrayList<>();
+
+    public void addSongs(String namePlayList, ArrayList<String> idSongs){
+        customList.add(namePlayList);
+        for (String id : idSongs) {
+            for (ArrayList cancion: libraryToShow) {
+                if (cancion.get(0).equals(id)){
+                    customList.add(cancion);
+                }
+            }
+        }
+    }
+    public ArrayList<ArrayList<String>> seePlayList(){
+        return customList;
+    }
+
+    @Override
+    public void playSongList() {
+        for (int i = 1; i < customList.size(); i++) {
+            System.out.println( "Reproduciendo..." + customList.get(i));
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
